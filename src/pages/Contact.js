ab9844aca27087b31233e2f20fdc2a05135a340e
anaisalfonsi <alfonsi_anais@hotmail.fr>
2021-02-01 11:36:03 +0100

    contact form + upload file for netlify added

---
+++ src/pages/Contact.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { navigateTo } from "gatsby-link";
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="text-black">Contact</h1>
+        <form
+          name="Contact Form"
+          method="POST"
+          data-netlify="true"
+          action="/thanks/"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <input type="hidden" name="form-name" value="Contact Form" />
+          <div>
+            <label>Your Email:</label>
+            <input type="email" name="email" />
+          </div>
+          <div>
+            <label>Message:</label>
+            <textarea name="message" />
+          </div>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    )
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,23 @@
-import React from "react";
-import Global from "../styles/global.css";
+import React from "react"
+import Link from "gatsby-link"
+//import Global from "../styles/global.css";
 
-export default function Home() {
-  return <div><h1 className="bg-purple-400 text-white text-center text-2xl">Hello world! ouat </h1></div>
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1 className="bg-purple-400 text-white text-center text-2xl">
+        testing contact form
+      </h1>
+      <ul>
+        <li>
+          <Link to="/Contact/">Basic contact form</Link>
+        </li>
+        <li>
+          <Link to="/file-upload/">Form with file upload</Link>
+        </li>
+      </ul>
+    </div>
+    ) 
+  }
 }
